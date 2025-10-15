0c85d2feaed85053835a3c7f2f0438b239413a4d
Luiz-Farias-dev <luizferreirafariasn@gmail.com>
2025-10-15 14:20:48 -0300

    Finalização de configuração do authorization service

--- src/main/java/com/grupocortez/almoxarifado_cortez/service/AuthorizationService.java
+++ src/main/java/com/grupocortez/almoxarifado_cortez/service/AuthorizationService.java
@@ -1,4 +1,24 @@
 package com.grupocortez.almoxarifado_cortez.service;
 
-public class AuthorizationService {
+import com.grupocortez.almoxarifado_cortez.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    UsuarioRepository repository;
+
+    AuthorizationService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    AuthorizationService() {}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByCpf(username);
+    }
 }
