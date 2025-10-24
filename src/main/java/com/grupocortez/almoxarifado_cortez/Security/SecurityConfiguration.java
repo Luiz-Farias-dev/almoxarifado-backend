b93f85eca74af87c1f5230d77458e6133bbbf15e
Luiz-Farias-dev <luizferreirafariasn@gmail.com>
2025-10-24 14:46:52 -0300

    authorizeHttpRequests adicionado

--- src/main/java/com/grupocortez/almoxarifado_cortez/Security/SecurityConfiguration.java
+++ src/main/java/com/grupocortez/almoxarifado_cortez/Security/SecurityConfiguration.java
@@ -1,7 +1,8 @@
-package com.grupocortez.almoxarifado_cortez.Security;
+package com.grupocortez.almoxarifado_cortez.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "produto-teste").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
