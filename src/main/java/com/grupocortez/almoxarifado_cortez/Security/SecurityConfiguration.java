dbd6cfa01eb6c5826ed7dbfd8f0d10a1e20c53d4
LuNe137 <luizferreirafariasn@gmail.com>
2025-10-28 23:52:40 -0300

    adicionando password encoder

--- src/main/java/com/grupocortez/almoxarifado_cortez/Security/SecurityConfiguration.java
+++ src/main/java/com/grupocortez/almoxarifado_cortez/Security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.grupocortez.almoxarifado_cortez.security;
+package com.grupocortez.almoxarifado_cortez.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +31,10 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
